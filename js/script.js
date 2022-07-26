575ed39b23145348bac39e0248d296282a38de10
MarianoTorres <marianotorresdistefano@gmail.com>
2022-07-26 16:20:19 -0300

    Segunda entrega proyecto final - Incompleto

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const registerSection = document.getElementById("registerSection")
 const registerForm = document.getElementById("registerForm")
 
 class User {
-    constructor(email, password, name, surname, birthday, country, gender){
+    constructor(email, password, name, surname, birthday, country, gender, sesionActive){
         this.email = email
         this.password = password
         this.name = name
@@ -10,31 +10,31 @@ class User {
         this.birthday = birthday
         this.country = country
         this.gender = gender
+        this.sesionActive = sesionActive
     }
 }
 
 const users = []
+let usersData = []
 
-registerForm.addEventListener("submit", (event) => {
-    event.preventDefault()
+if(localStorage.getItem("usersData")){
+    usersData = JSON.parse(localStorage.getItem("usersData"))
+} else {
+    localStorage.setItem("usersData", JSON.stringify(usersData))
+}
 
-    let email = document.getElementById("registerEmail").value
-    let password = document.getElementById("registerPassword").value
-    let name = document.getElementById("registerName").value
-    let surname = document.getElementById("registerSurname").value
-    let birthday = document.getElementById("registerBirthday").value
-    let country = document.getElementById("registerCountry").value
-    let gender = document.querySelector('input[name="gender"]:checked').value
+registerForm.addEventListener("submit", (e) => {
+    e.preventDefault()
 
-    const user = new User(email, password, name, surname, birthday, country, gender)
+    let datForm = new FormData(e.target)
+
+    const user = new User(datForm.get("email"), datForm.get("password"), datForm.get("name"), datForm.get("surname"), datForm.get("birthday"), datForm.get("country"), datForm.get("gender"), true)
     users.push(user)
 
-    registerSection.innerHTML = `
-        <div id="registerSuccessful" class="col-8 col-md-9 col-xl-10 m-auto d-flex flex-column justify-content-around my-5">
-            <h2 class="titulo-seccion text-center my-5 pb-3">¡Gracias por registrarte en SportStyle!</h2>
-            <h3 class="text-center mb-xl-4">${user.name} se ha enviado un correo a tu casilla: ${user.email}.</h3>
-            <p class="text-center">Una vez realices la confirmación podrás acceder con tu email y contraseña para disfrutar de todos nuestros beneficios</p>
-        </div>
-    
-    `
+    usersData = users.map(user => user = {email: user.email, nombre: user.name, sesion: user.sesionActive})
+    localStorage.setItem("usersData", JSON.stringify(usersData))
+
+    registerForm.reset()
+
+    console.log(users)
 })
