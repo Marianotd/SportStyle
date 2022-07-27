774464424239110ddebdfb98fa836ab62b0db761
MarianoTorres <marianotorresdistefano@gmail.com>
2022-07-27 15:58:38 -0300

    Prueba con Modal hecho con JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-const registerSection = document.getElementById("registerSection")
+// REGISTRAR EN VARIABLES ELEMENTOS DEL DOM
 const registerForm = document.getElementById("registerForm")
+const abrirModalRegistro = document.getElementById("abrirModalRegistro")
+const modalRegistro = document.querySelector(".modalRegistro")
+const cerrarModalRegistro = document.querySelector(".cerrarModalRegistro")
 
+
+// CLASE USUARIO
 class User {
     constructor(email, password, name, surname, birthday, country, gender, sesionActive){
         this.email = email
@@ -14,20 +19,22 @@ class User {
     }
 }
 
+// ARRAYS
 const users = []
 let usersData = []
 
+// REVISAR LOCAL STORAGE
 if(localStorage.getItem("usersData")){
     usersData = JSON.parse(localStorage.getItem("usersData"))
 } else {
     localStorage.setItem("usersData", JSON.stringify(usersData))
 }
 
+// EVENTO FORMULARIO REGISTRO
 registerForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     let datForm = new FormData(e.target)
-
     const user = new User(datForm.get("email"), datForm.get("password"), datForm.get("name"), datForm.get("surname"), datForm.get("birthday"), datForm.get("country"), datForm.get("gender"), true)
     users.push(user)
 
@@ -36,14 +43,25 @@ registerForm.addEventListener("submit", (e) => {
 
     registerForm.reset()
 
-    console.log(users)
-
-    registerSection.innerHTML = `
-        <div class="col-10 col-md-8 col-xl-9 m-auto d-flex flex-column justify-content-around my-5">
-            <h2 class="titulo-seccion text-center my-5 pb-3">¡Gracias por registrarte en SportStyle!</h2>
-            <h3 class="text-center mb-xl-4">${user.name} se ha enviado un correo a tu casilla: ${user.email}</h3>
-            <p class="text-center">Una vez realices la confirmación podrás acceder con tu email y contraseña para disfrutar de todos nuestros beneficios</p>
+    modalRegistro.innerHTML = `
+        <div class="modalContenedor">
+            <h2 class="modalTitulo">HOLA</h2>
+            <div>
+                <p class="modalTexto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore iure dolor veniam expedita fugit earum sed, voluptatum repellendus ab numquam beatae quibusdam consequuntur ut aperiam a quo totam eligendi deleniti? Corporis, ipsa vero odit, officia ullam delectus, fugiat quam eligendi accusantium quos assumenda tenetur tempora eveniet est necessitatibus dolorum similique aspernatur vel nisi distinctio possimus voluptatem autem. Nisi, debitis accusantium placeat cupiditate aut perferendis perspiciatis natus soluta similique itaque explicabo ullam quod totam. Sequi voluptas, voluptatibus dolorum odit itaque consectetur illum architecto incidunt cum iste placeat ducimus enim libero exercitationem dicta. Impedit corporis velit iste voluptates soluta fugiat possimus mollitia?</p>
+            </div>
+            <button class="cerrarModalRegistro" type="button">Cerrar Modal</button>
         </div>
 
     `   
+
+    modalRegistro.classList.add("modalRegistro--show")
+
+    cerrarModalRegistro.addEventListener("click", ( e) => {
+        e.preventDefault()
+        modalRegistro.classList.remove("modalRegistro--show")
+    })
 })
+
+
+
+
