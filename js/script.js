2dc9d0c19513d6e333db7c52e73465670e6f697b
MarianoTorres <marianotorresdistefano@gmail.com>
2022-08-04 16:47:10 -0300

    Pruebas con libreria para carousel

--- js/script.js
+++ js/script.js
@@ -36,11 +36,11 @@ const producto6 = new Producto(6, "Pantalón Nike Dri-FIT Strike 21", "Nike", 12
 const producto7 = new Producto(7, "Zapatillas Gel-Equation 11", "Asics", 17490, "media/hombre/productos/ZapatillasGelEquation11.png", "Hombre", "Calzado", talleCalzado, 2, true, false, 0)
 const producto8 = new Producto(8, "Remera Sin Mangas Back", "Ascis", 4800, "media/hombre/productos/RemeraSinMangaBack.png", "Hombre", "Remera", talleIndumentaria, 20, true, false, 0)
 const producto9 = new Producto(9, "Zapatillas Superstar", "Adidas", 20999, "media/img/productos/ZapatillasSuperstar.png", "Hombre", "Calzado", talleCalzado, 60, true, true, 0)
-const producto10 = new Producto(10, "Zapatillas Gel Hypersonic 2 W Asics", "Asics", 20489, "media/mujer/productos/ZapatillasGelHypersonic2WAsics.jpg", " Mujer", "Calzado", talleCalzado, 9, true, false, 0)
+const producto10 = new Producto(10, "Zapatillas Gel Hypersonic 2 W Asics", "Asics", 20489, "media/mujer/productos/ZapatillasGelHypersonic2WAsics.jpg", " Mujer", "Calzado", talleCalzado, 9, true, true, 0)
 const producto11 = new Producto(11, "Remera En V Neck Ss Asics", "Asics", 5869, "media/mujer/productos/RemeraEnVNeckSsAsics.jpg", "Mujer", "Remera", talleIndumentaria, 50, true, false, 0)
 const producto12 = new Producto(12, "Botella Sportstyle Puma", "Puma", 4199, "media/mujer/productos/BotellaSportStylePuma.jpg", "Todos" , "Accesorio", null, 14, true, false, 0)
 const producto13 = new Producto(13, "Zapatillas Forum Low", "Adidas", 23999, "media/img/productos/ZapatillasForumLow.png", "Hombre", "Calzado", talleCalzado, 12, true, false, 0)
-const producto14 = new Producto(14, "Remera Mujer Match Ii Fila", "Fila", 2689, "media/mujer/productos/RemeraMujerMatchLiFila.jpg", "Mujer", "Remera", talleIndumentaria, 25, true, false, 0)
+const producto14 = new Producto(14, "Remera Mujer Match Ii Fila", "Fila", 2689, "media/mujer/productos/RemeraMujerMatchLiFila.jpg", "Mujer", "Remera", talleIndumentaria, 25, true, true, 0)
 const producto15 = new Producto(15, "Zapatillas Trend 2.0 W Fila", "Fila", 9489, "media/mujer/productos/ZapatillasTrend20WFila.jpg", "Mujer", "Calzado", talleCalzado, 1, true, false, 0)
 const producto16 = new Producto(16, "Campera Deportiva Favorites Tt Wv adidas", "Adidas", 14429, "media/mujer/productos/CamperaDeportivaFavoritesTtWvadidas.jpg", "Mujer", "Campera", talleIndumentaria, 12, true, true, 0)
 
@@ -50,12 +50,12 @@ const productos = [producto1, producto2, producto3, producto4, producto5, produc
 productos.forEach(producto => {
 
     if(producto.destacado == true){
-        carouselProductos.innerHTML += `
-        <div id="producto${producto.id}" class="producto col-8 col-md-6 col-xl-2">
-            <p>${producto.nombre} <br> $${producto.precio}</p>
-            <img class="img-fluid" src="${producto.img}" alt="${producto.nombre}">
-            <a class="py-1 px-4 rounded" data-bs-toggle="offcanvas" href="#carrito" role="button" aria-controls="carrito-sm">Añadir</a>
-        </div>
-    `
+            carouselProductos.innerHTML += `
+            <div id="producto${producto.id}" class="producto col-8 col-md-6 col-xl-2">
+                <p>${producto.nombre} <br> $${producto.precio}</p>
+                <img class="img-fluid" src="${producto.img}" alt="${producto.nombre}">
+                <a class="py-1 px-4 rounded" data-bs-toggle="offcanvas" href="#carrito" role="button" aria-controls="carrito-sm">Añadir</a>
+            </div>
+        `
     }
 })
