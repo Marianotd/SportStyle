e7f2ad192cb383301109ccb8807b3ff4734d5d41
MarianoTorres <marianotorresdistefano@gmail.com>
2022-08-17 15:50:07 -0300

    Trabajos carrito

--- js/script.js
+++ js/script.js
@@ -25,8 +25,8 @@ async function mostrarProductos() {
                     <div id="producto${producto.id}" class="card border-0 producto">
                         <p>${producto.nombre} <br> $${producto.precio}</p>
                         <img class="img img-fluid" src="${producto.img}" alt="${producto.nombre}">
-                        <label class="labelTalle col-12 mb-2" for="talleCarrito">Seleccione el talle:</label>
-                        <select id="talleCarrito" name="talleCarrito" class="col-6 px-2 py-1" required></select>
+                        <label id="labelTalle${producto.id}" class="labelTalle col-12 mb-2" for="talleProducto">Seleccione el talle:</label>
+                        <select id="talleProducto${producto.id}" name="talleProducto" class="col-10 ps-3 py-2" required></select>
                         <button class="py-1 px-4 rounded">Añadir</button>
                     </div>
                 </li>
@@ -38,8 +38,8 @@ async function mostrarProductos() {
                 <div id="producto${producto.id}" class="card border-0 producto producto__catalogo">
                     <p>${producto.nombre} <br> $${producto.precio}</p>
                     <img class="img img-fluid" src="${producto.img}" alt="${producto.nombre}">
-                    <label class="labelTalle col-12 mb-2" for="talleCarrito">Seleccione el talle:</label>
-                    <select id="talleCarrito" name="talleCarrito" class="col-6 px-2 py-1" required></select>
+                    <label id="labelTalle${producto.id}" class="labelTalle col-12 mb-2" for="talleProducto">Seleccione el talle:</label>
+                    <select id="talleProducto${producto.id}" name="talleProducto" class="col-10 ps-3 py-2" required></select>
                     <button id="botonProducto${producto.id}" class="py-1 px-4 rounded">Añadir</button>
                 </div>
             `
@@ -50,8 +50,8 @@ async function mostrarProductos() {
                 <div id="producto${producto.id}" class="card border-0 producto producto__catalogo producto--mujer ">
                     <p>${producto.nombre} <br> $${producto.precio}</p>
                     <img class="img img-fluid" src="${producto.img}" alt="${producto.nombre}">
-                    <label class="labelTalle col-12 mb-2" for="talleCarrito">Seleccione el talle:</label>
-                    <select id="talleCarrito" name="talleCarrito" class="col-6 px-2 py-1" required></select>
+                    <label id="labelTalle${producto.id}" class="labelTalle col-12 mb-2" for="talleProducto">Seleccione el talle:</label>
+                    <select id="talleProducto${producto.id}" name="talleProducto" class="col-10 ps-3 py-2" required></select>
                     <button id="botonProducto${producto.id}" class="py-1 px-4 rounded">Añadir</button>
                 </div>
             `
@@ -63,8 +63,8 @@ async function mostrarProductos() {
                     <div id="producto${producto.id}" class="card border-0 producto producto--niños">
                         <p>${producto.nombre} <br> $${producto.precio}</p>
                         <img class="img img-fluid" src="${producto.img}" alt="${producto.nombre}">
-                        <label class="labelTalle col-12 mb-2" for="talleCarrito">Seleccione el talle:</label>
-                        <select id="talleCarritos" name="talleCarrito" class="col-6 px-2 py-1" required></select>
+                        <label id="labelTalle${producto.id}" class="labelTalle col-12 mb-2" for="talleProducto">Seleccione el talle:</label>
+                        <select id="talleProducto${producto.id}" name="talleProducto" class="col-10 ps-3 py-2" required></select>
                         <button class="py-1 px-4 rounded">Añadir</button>
                     </div>
                 </li>
@@ -75,64 +75,140 @@ async function mostrarProductos() {
     prodParseados.forEach((producto) => {
         if(document.getElementById(`producto${producto.id}`)){    
             botonAgregarCarrito(producto)
+            completarTalles(producto)
         }
     })
 }
 
 // FUNCIÓN LLENAR TALLES
 function completarTalles(producto){
+    const selectProductos = document.getElementById(`talleProducto${producto.id}`)
+    const labelTalle = document.getElementById(`labelTalle${producto.id}`)
 
+    if(producto.categoria !== "Accesorio"){
+        if(producto.categoria !== "Calzado" && producto.categoria !== null){
+            selectProductos.innerHTML = `
+                <option value="" disabled selected>Seleccione un talle</option>
+                <option value="XS">XS</option>
+                <option value="S">S</option>
+                <option value="M">M</option>
+                <option value="L">L</option>
+                <option value="XL">XL</option>
+                <option value="XXL">XXL</option>
+            `
+        } else if(producto.categoria == "Calzado" && producto.categoria !== null && producto.usuario == "Hombre"){
+            selectProductos.innerHTML = `
+                <option value="" disabled selected>Seleccione un talle</option>
+                <option value="35">35</option>
+                <option value="36">36</option>
+                <option value="37">37</option>
+                <option value="38">38</option>
+                <option value="39">39</option>
+                <option value="40">40</option>
+                <option value="41">41</option>
+            `
+        } else if(producto.categoria == "Calzado" && producto.categoria !== null && producto.usuario == "Mujer"){
+            selectProductos.innerHTML = `
+                <option value="" disabled selected>Seleccione un talle</option>
+                <option value="38">38</option>
+                <option value="39">39</option>
+                <option value="40">40</option>
+                <option value="41">41</option>
+                <option value="42">42</option>
+                <option value="43">43</option>
+                <option value="44">44</option>
+                <option value="45">45</option>
+                <option value="46">46</option>
+            `
+        } else if(producto.categoria == "Calzado" && producto.categoria !== null && producto.usuario == "Niños"){
+            selectProductos.innerHTML = `
+                <option value="" disabled selected>Seleccione un talle</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+                <option value="21">21</option>
+                <option value="22">22</option>
+                <option value="23">23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+                <option value="27">27</option>
+                <option value="28">28</option>
+                <option value="29">29</option>
+                <option value="30">30</option>
+                <option value="31">31</option>
+                <option value="32">32</option>
+                <option value="33">33</option>
+                <option value="34">34</option>
+                <option value="35">35</option>
+                <option value="36">36</option>
+                <option value="37">37</option>
+                <option value="38">38</option>
+            `
+        }
+    } else {
+        selectProductos.classList.add("d-none")
+        labelTalle.classList.add("d-none")
+    }
 }
 
 //FUNCIÓN AÑADIR AL CARRITO
 function botonAgregarCarrito(producto){ 
     document.getElementById(`producto${producto.id}`).lastElementChild.addEventListener('click', () => {
 
-        let prodId = producto.id
+        const selectProductos = document.getElementById(`talleProducto${producto.id}`)
 
-        if(carrito.some(producto => producto.id == prodId)){
-            carrito[producto.cantidad++]
-        } else {
-            carrito.push(producto)
-            producto.cantidad++
-        }
+        if(selectProductos.value !== "" || selectProductos.classList.contains("d-none")){
+            let prodId = producto.id
 
-        if(carrito !== []){
-            localStorage.setItem("carrito", JSON.stringify(carrito.map(producto => producto = {id: producto.id, cant: producto.cantidad})))                        
-        }
-    
-        divCarrito.innerHTML = ""
-        divCarrito.innerHTML += `
-            <div class="modalCarrito">
-                <div class="carritoHeader d-flex flex-row flex-nowrap justify-content-around align-items-center">
-                    <h3 class="m-0 p-0">¡Producto añadido al carrito!</h3>
-                    <button class="col-2" type="button" onClick="eliminarCarrito(${producto.id})">X</button>
-                </div>
+            if(carrito.some(producto => producto.id == prodId)){
+                carrito[producto.cantidad++]
+            } else {
+                carrito.push(producto)
+                producto.cantidad++
+            }
 
-                <div class="carritoBody d-flex flex-column align-items-center justify-content-around bg-light py-4 px-1 mt-3">
-                    <div class="carritoProducto d-flex flex-row flex-nowrap justify-content-around">
-                        <img src="${producto.img}" class="img-fluid col-5" alt="${producto.nombre}">
-                        <div class="ps-1">
-                            <p>${producto.nombre} <br> <span>$ ${producto.precio}</span></p>
+            if(carrito !== []){
+                localStorage.setItem("carrito", JSON.stringify(carrito.map(producto => producto = {id: producto.id, cant: producto.cantidad})))                        
+            }
+        
+            divCarrito.innerHTML = ""
+            divCarrito.innerHTML += `
+                <div class="modalCarrito">
+                    <div class="carritoHeader d-flex flex-row flex-nowrap justify-content-around align-items-center">
+                        <h3 class="m-0 p-0">¡Producto añadido al carrito!</h3>
+                        <button class="col-2" type="button" onClick="eliminarCarrito(${producto.id})">X</button>
+                    </div>
+
+                    <div class="carritoBody d-flex flex-column align-items-center justify-content-around bg-light py-4 px-1 mt-3">
+                        <div class="carritoProducto d-flex flex-row flex-nowrap justify-content-around">
+                            <img src="${producto.img}" class="img-fluid col-5" alt="${producto.nombre}">
+                            <div class="ps-1">
+                                <p>${producto.nombre} <br> <span>$ ${producto.precio}</span></p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="carritoFooter col-12 d-flex flex-column align-items-center justify-content-evenly gap-2 mt-4">
-                        <a class="col-10 mx-auto py-2 px-3 bg-black text-decoration-none text-white" href="carrito.html">Ver carrito</a>
-                        <button class="col-10 mx-auto py-2 px-3 bg-white text-start" type="button" onClick="seguirComprando(${producto.id})">Añadir y seguir comprando</button>
-                    </div>
-                </div> 
-            </div>
-        `
+                        <div class="carritoFooter col-12 d-flex flex-column align-items-center justify-content-evenly gap-2 mt-4">
+                            <a class="col-10 mx-auto py-2 px-3 bg-black text-decoration-none text-white" href="carrito.html">Ver carrito</a>
+                            <button class="col-10 mx-auto py-2 px-3 bg-white text-start" type="button" onClick="seguirComprando(${producto.id})">Añadir y seguir comprando</button>
+                        </div>
+                    </div> 
+                </div>
+            `
 
-        console.log(document.getElementById(`talleCarrito${producto.id}`))
+            divCarrito.classList.add("divModal--show")
+            document.body.classList.add("body--modal")
+            selectProductos.classList.remove("talleIncorrecto")
+            selectProductos.value = ""
 
-        divCarrito.classList.add("divModal--show")
-        document.body.classList.add("body--modal")
-
-        if(filename() == "index.html"){
-            const secOfertas = document.getElementById("secOfertas")
-            secOfertas.classList.remove("sticky-top")
+            if(filename() == "index.html"){
+                const secOfertas = document.getElementById("secOfertas")
+                secOfertas.classList.remove("sticky-top")
+            }
+        } else {
+            selectProductos.classList.add("talleIncorrecto")
         }
     })
 }
