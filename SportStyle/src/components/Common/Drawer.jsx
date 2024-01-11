e363dd009a53b8d5257120d327454a6f00fb74ea
MarianoTorres <marianotorresdistefano@gmail.com>
2024-01-11 14:01:34 -0300

    Rework navbar con drawer

--- SportStyle/src/components/Common/Drawer.jsx
+++ SportStyle/src/components/Common/Drawer.jsx
@@ -1,20 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Drawer({ active }) {
+export default function Drawer({ active, links }) {
     const [heightDrawer, setHeightDrawer] = useState(window.innerHeight)
     const [widthDrawer, setWidthDrawer] = useState(window.innerWidth)
-
-    const listItems = [
-        { text: 'Productos', link: 'productos' },
-        { text: 'Hombre', link: 'hombre' },
-        { text: 'Mujer', link: 'mujer' },
-        { text: 'Niños', link: 'niños' },
-        { text: 'Nosotros', link: 'nosotros' },
-        { text: 'Contacto', link: 'contacto' },
-        { text: 'Iniciar Sesión', link: 'login', size: 'small' },
-        { text: 'Registrarse', link: 'register', size: 'small' }
-    ]
 
     const drawerStyleVisible = {
         left: 0,
@@ -36,10 +25,10 @@ export default function Drawer({ active }) {
 
   return (
     <div className={'drawer'} style={active ? drawerStyleVisible : drawerStyleInvisible}>
-        { listItems.map(item => {
+        { links.map(item => {
             return(
-                <Link to={`/${item.link}`} className={item.size != 'small' ? 'listItem' : 'listItem listItem--small'} key={item.text}>
-                    {item.text}
+                <Link to={`/${item.url}`} className={item.size != 'small' ? 'listItem' : 'listItem listItem--small'} key={item.name}>
+                    {item.name}
                 </Link>
             )
         })}
--- SportStyle/src/components/Common/NavBar.jsx
+++ SportStyle/src/components/Common/NavBar.jsx
@@ -5,10 +5,11 @@ import { RiShoppingCart2Line } from 'react-icons/ri';
 import { Link, useLocation } from 'react-router-dom';
 import Drawer from './Drawer.jsx';
 import MenuButton from './MenuButton.jsx';
+import LinkList from './LinkList'
 
 export default function NavBar() {
   const [menu, setMenu] = useState(false)
-  const { pathname } = useLocation()
+  const { pathname } = useLocation() 
 
   useEffect(() => {
     setMenu(false)
@@ -18,6 +19,17 @@ export default function NavBar() {
     const newMenu = !menu
     setMenu(newMenu)
   }
+
+  const listItems = [
+    { name: 'Productos', url: 'productos', class: 'navLink' },
+    { name: 'Hombre', url: 'hombre', class: 'navLink' },
+    { name: 'Mujer', url: 'mujer', class: 'navLink' },
+    { name: 'Niños', url: 'niños', class: 'navLink' },
+    { name: 'Nosotros', url: 'nosotros', class: 'navLink' },
+    { name: 'Contacto', url: 'contacto', class: 'navLink' },
+    { name: 'Iniciar Sesión', url: 'login', class: 'navLink navLink--esp', size: 'small' },
+    { name: 'Registrarse', url: 'register', class: 'navLink navLink--esp', size: 'small' }
+  ]
 
   return (
     <header>
@@ -34,7 +46,11 @@ export default function NavBar() {
           </Link>
       </div>
 
-      <Drawer active={menu}/>
+      <Drawer active={menu} links={listItems}/>
+
+      <div className={`navMenu`}>
+        <LinkList links={listItems}/>
+      </div>
     </header>
   )
 }
